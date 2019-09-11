0177bcc41980d923daa558a4a985fe4d0d4b9afc
Ingo Andelhofs <Ingom2000@gmail.com>
2019-09-11 17:02:01 +0200

    Created an advanced class that can take an assoc array and access it like an Object.

--- _modules/helpers/GetNullObj.php
+++ _modules/helpers/GetNullObj.php
@@ -13,3 +13,35 @@ class GetNullObj {
       return $o;
   }
 }
+
+
+class AdvancedNullObject {
+  public $assoc_array;
+
+  public function __construct(&$assoc_array = null) {
+    $this->assoc_array = &$assoc_array;
+  }
+
+  public function __get($key) {
+    $value = &$this->assoc_array[$key] ?? null;
+    return (is_array($value) && array_values($value) !== $value) ? new AdvancedNullObject($value) : $value;
+  }
+
+  public function __set($key, $value) {
+    $this->assoc_array[$key] = $value;
+  }
+
+  public function &get_array() {
+    return $this->assoc_array;
+  }
+};
+
+// TEST CASES
+// $assoc_ref = ['test' => ['test' => 3]];
+// $obj = new AdvancedNullObject($assoc_ref);
+// $assoc2 = &$obj->get_array();
+// $obj->test->more = 'testtttt2';
+// print_r($obj->test->more); echo '<br>';
+// print_r($obj->assoc_array); echo "<br>";
+// print_r($assoc2); echo "<br>";
+// print_r($assoc_ref); echo "<br>";
