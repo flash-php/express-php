bfce02fff5fa63cec06620e0df4d302de1403b9d
Ingo Andelhofs <ingom2000@gmail.com>
2019-10-01 21:28:35 +0200

    Revert "Moved application folders to the app folder." This reverts commit 98dae7a5
    (Stopped syncing GitBook)

--- _modules/config/private/http.php
+++ _modules/config/private/http.php
@@ -1,12 +1,12 @@
 <?php
 
 // Router advanced
-Config::default('PATH_COMPONENTS', './app/components');
-Config::default('PATH_MIDDLEWARE', './app/middleware');
-Config::default('PATH_MODELS', './app/models');
-Config::default('PATH_ROUTES', './app/routes');
-Config::default('PATH_TEMPLATES', './app/templates');
-Config::default('PATH_VIEWS', './app/views');
+Config::default('PATH_COMPONENTS', './components');
+Config::default('PATH_MODELS', './models');
+Config::default('PATH_VIEWS', './views');
+Config::default('PATH_TEMPLATES', './templates');
+Config::default('PATH_ROUTES', './routes');
+Config::default('PATH_MIDDLEWARE', './middleware');
 
 Config::default('DEFAULT_ROUTER_METHOD', 'index');
 Config::default('DEFAULT_ROUTER_ROUTE', 'home');
